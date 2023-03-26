c71fc0b75df5512cbc77f612580696c012eb00df
HicaroD <hdanrlley1@gmail.com>
2023-03-26 19:25:48 -0300

    adjlist: no implementations yet

---
+++ adjacency_list.py
@@ -0,0 +1,10 @@
+# TODO
+class AdjacencyList:
+    def __init__(self, number_of_vertices: int) -> None:
+        pass
+
+    def add_vertice(self):
+        pass
+
+    def remove_vertice(self):
+        pass
