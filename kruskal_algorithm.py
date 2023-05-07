c780e9c0918b80b85c7c38af7035923c97a2952e
HicaroD <hdanrlley1@gmail.com>
2023-05-07 15:06:09 -0300

    kruskal: adding implementation without using union find

---
+++ kruskal_algorithm.py
@@ -0,0 +1,49 @@
+from typing import List, Tuple
+
+
+class Kruskal:
+    def __init__(self, graph: dict) -> None:
+        self.graph = graph
+
+    def get_sorted_edges(self) -> List[Tuple[str, str, int]]:
+        edges = []
+        for start_vertex, neighbors in self.graph.items():
+            for end_vertex, weight in neighbors.items():
+                edges.append((start_vertex, end_vertex, weight))
+        return sorted(edges, key=lambda edge: edge[2])
+
+    def find_minimum_spanning_tree(
+        self,
+    ) -> List[Tuple[int, int, int]]:
+        sorted_edges = self.get_sorted_edges()
+        parent = [i for i in range(len(self.graph))]
+        minimum_spanning_tree = []
+
+        for u, v, weight in sorted_edges:
+            parent_u = self.find_parent(parent, int(u) - 1)
+            parent_v = self.find_parent(parent, int(v) - 1)
+            if parent_u != parent_v:
+                minimum_spanning_tree.append((u, v, weight))
+                parent[parent_u] = parent_v
+        return minimum_spanning_tree
+
+    def find_parent(self, parent: List[int], node: int) -> int:
+        if parent[node] == node:
+            return node
+        return self.find_parent(parent, parent[node])
+
+
+def main():
+    graph = {
+        "1": {"2": 1, "3": 3},
+        "2": {"3": 2, "4": 10, "5": 7},
+        "3": {"5": 4},
+        "4": {"5": 2},
+        "5": {},
+    }
+    kruskal = Kruskal(graph)
+    print(kruskal.find_minimum_spanning_tree())
+
+
+if __name__ == "__main__":
+    main()
